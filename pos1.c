8628a1364dc7afd4cc464592df0c0e62bef1892f
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-08 10:58:00 +0530

    Create pos1.c

---
+++ pos1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h?
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a==0)
+printf("number is zero");
+elseif(a>0)
+printf("number is positive");
+elseif(a<0)
+printf("number is negative");
+getch();
+}
