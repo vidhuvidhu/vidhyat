484ce77286bac808eabb6a135666ee47c3ccf0f3
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-08 11:17:25 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+char n;
+scanf("%d"&a);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+printf("print vowel");
+else
+printf("print consonent");
+getch();
+}
