a5f3325e4970d9f19b9e2ddcf3ca9e90479cfab5
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-27 16:03:04 +0530

    Create oddinterval.c

---
+++ oddinterval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d is a odd number",i);
+        }
+      
+    }
+        return 0;
+    }
+
+    
