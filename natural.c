0f210fe234e99a2779e42e9b9851864cf900d4e5
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-27 11:30:43 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,i=0;
+        scanf("%d",&n);
+    while(i<=n)
+    {
+        sum=sum+i;
+    i++;
+    }
+    printf("the sum is %d",sum);
+    return 0;
+}
