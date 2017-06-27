84cc3cd6b11a483902a19a9b21304807bfdbbcdf
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-27 12:35:54 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -4,16 +4,19 @@ int main()
 int rev,org,n,i=0;
 scanf("%d",&n);
 org=n;
+  while(n!=0)
+  {
 rev=n%10;
 i=i*10+r;
 n/=10;
-if(rev=org)
+  }
+if(i==org)
 {
 printf("%d is palindrome",org);
 }
 else
 {
-printf("d is not a palindrome",org)'
+printf("%d is not a palindrome",org);
 }
 return 0;
 }
