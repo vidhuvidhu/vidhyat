bad30c487b7c9813b1ecceede4a7a57e7b3fd476
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-28 15:55:45 +0530

    Create H.C.F.c

---
+++ H.C.F.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i, hcf;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+    
+        if(n1%i==0 && n2%i==0)
+            hcf= i;
+    }
+
+    printf("H.C.F of %d and %d is %d", n1, n2, hcf);
+
+    return 0;
+}
