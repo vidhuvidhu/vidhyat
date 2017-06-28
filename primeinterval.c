c426c0dfd5d5fc3a7d7bef1d73d0f36acf938894
vidhuvidhu <vidhyat1996@gmail.com>
2017-06-28 10:18:11 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+ 
+int main()
+{
+    int n, i, j, min, max, flag=0;
+     
+    printf("\n Enter two numbers(Intervals) : ");
+    scanf("%d%d",&min,&max);
+     
+    printf("\n The prime numbers between %d and %d are :",min,max);
+     
+    for(i=min+1;i<max;i++)
+    {
+        flag=0;
+         
+        for(j=2;j<=sqrt(i);j++)      
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+         
+        if (flag==0)
+        {
+            printf(" %d ",i);
+        }
+    }
+    return 0;
+}
